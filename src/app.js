57341faab8d8b3650108d781fff00b0473015132
Evelin Romero <Evelinrocio2816@gmail.com>
2024-03-13 19:18:08 -0300

    coorrecciones

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require("express")
 const app =express();
-const imageRoutes= require("./routes/image.router.js")
 const exphds= require("express-handlebars")
 const multer= require("multer")
+
 const PORT = 8080;
+
+const imageRoutes= require("./routes/image.router.js")
 require("../src/db/database.js")
 
 //Handlebars
@@ -16,13 +18,14 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(express.static("./src/public"))
 
+
 //multer
 const storage = multer.diskStorage({
     destination:(req, file, cb)=>{
         cb(null, "./src/public/img")
     },
     filename:(req, file, cb)=>{
-        cb:(null, file.originalname)
+        cb(null, file.originalname)
     }
 })
 app.use(multer({storage}).single("image"))
